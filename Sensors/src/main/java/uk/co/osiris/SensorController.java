77532ac595f67fbe700aa12424296c8d9e335de5
Adrian Challinor <adrian.challinor@osiris.co.uk>
2023-01-26 18:13:32 +0000

    Add debug

--- Sensors/src/main/java/uk/co/osiris/SensorController.java
+++ Sensors/src/main/java/uk/co/osiris/SensorController.java
@@ -5,9 +5,11 @@ import java.util.ArrayList;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
 import uk.co.osiris.config.Sensor;
 
 @RestController
+@Slf4j
 public class SensorController {
 	private final SensorService svc;
 
@@ -17,6 +19,7 @@ public class SensorController {
 	
 	@GetMapping("/sensors")
 	public ArrayList<Sensor> configuration( ) { 
+		log.debug("Fat Controller probed for sensors");
 		return svc.getSensors();
 	}
 	
