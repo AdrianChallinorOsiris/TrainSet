37878d812a61c86a8aca7a7f2b639db98fe9288c
Adrian Challinor <adrian.challinor@osiris.co.uk>
2023-01-26 17:00:18 +0000

    Pins and needles

--- Sensors/src/main/java/uk/co/osiris/common/Utility.java
+++ Sensors/src/main/java/uk/co/osiris/common/Utility.java
@@ -15,7 +15,13 @@ public class Utility {
 			log.error("PIN was found to be null");
 			
 			Pin pin2 = RaspiPin.GPIO_02;
-			log.error("pin2 = {}", pin2);
+			log.error("pin2 = {}", pin2.getName());
+			
+			Pin pin3 = RaspiPin.getPinByName( pin2.getName());
+			log.error("Pin3 = {}", pin3);
+			
+			Pin pin4 = RaspiPin.getPinByAddress(04);
+			log.error("Pin4 = {}", pin4);
 			System.exit(0);
 		}
 		return pin;
